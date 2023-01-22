ca90c450a95c6746bf87f2e890055dfd95f255fb
LiterallyEthical <ibolaski55@hotmail.com>
2023-01-22 14:23:05 +0300

    aes_file_encyrption.py updated

--- aes_file_encryption.py
+++ aes_file_encryption.py
@@ -61,7 +61,7 @@ def decrypt_file(file_path, key_path):
         f.write(plaintext)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Encrypt a file, then decyrpt it whenever you need")    
+    parser = argparse.ArgumentParser(description="A simple command line tool that encrypts and decrypts files using AES encryption and stores the key in a separate file.")    
     parser.add_argument('file_path', type=str, help="File path to encrypt or decyrpt")
     parser.add_argument('key_path', type=str, help="File path to save the key or read the key")
     parser.add_argument('-d', '--decrypt', action='store_true',help="Option that switchs to decyrpt mode, default is encyrpt")
